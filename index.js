b6694d63fb00f3c27e230f723ee6024e0b1331fc
Nicholas <nicholas.nmalinda@gmail.com>
2022-06-29 12:45:49 +0300

    commit tree caca9805630ca4f78bf3b6219a77e12cb323ae6a

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charArt(0).toUpperCase + string.slice(1);
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-']+/g, "")
+  }
+  static titleize(sring){
+    const words = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let message = string.split("")
+    for(const word of words){
+      for(const item of message){
+        if(word !== item){
+          item.toUpperCase
+        }
+      }
+    }
+  }
 }
